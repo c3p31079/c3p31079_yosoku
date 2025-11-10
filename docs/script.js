355bf4dbfc6dee97f712af9df8ae5533c2a6f35f
c3p31079 <146411811+c3p31079@users.noreply.github.com>
2025-11-10 10:58:38 +0900

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -5,7 +5,7 @@ async function send() {
   const formData = new FormData();
   formData.append("file", file);
 
-  const res = await fetch("https://your-render-api.onrender.com/predict", {
+  const res = await fetch("https://c3p31079-yosoku.onrender.com", {
     method: "POST",
     body: formData
   });
